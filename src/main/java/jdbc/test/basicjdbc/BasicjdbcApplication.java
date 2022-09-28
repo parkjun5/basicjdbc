b7030c2fee56742ad03b85ae30f587be4087e324
박세준 <sjpark@selab.co.kr>
2022-09-28 16:47:15 +0900

    Initial commit

---
+++ src/main/java/jdbc/test/basicjdbc/BasicjdbcApplication.java
@@ -0,0 +1,13 @@
+package jdbc.test.basicjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicjdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicjdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/jdbc/test/basicjdbc/BasicjdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package jdbc.test.basicjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicjdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
