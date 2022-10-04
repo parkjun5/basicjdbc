a07bd5ec173527123de12b28ac0e764a1ea17c4e
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2022-10-04 21:45:11 +0900

    DBConnection Base

---
+++ src/main/java/jdbc/test/basicjdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package jdbc.test.basicjdbc.connection;
+
+public abstract class ConnectionConst {
+    public static final String URL = "jdbc:h2:tcp://localhost/~/jdbc_test";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
+}
---
+++ src/test/java/jdbc/test/basicjdbc/connection/DBConnectionUtilTest.java
@@ -0,0 +1,20 @@
+package jdbc.test.basicjdbc.connection;
+
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+class DBConnectionUtilTest {
+
+    @Test
+    void connectionTest() {
+        Connection connection = DBConnectionUtil.getConnection();
+        assertThat(connection).isNotNull();
+    }
+}
