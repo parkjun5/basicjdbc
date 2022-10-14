456e2a8c3d2bc7a7ce945e323e4f945f434d2820
박세준 <sjpark@selab.co.kr>
2022-10-14 18:18:32 +0900

    [ETC] polishing

--- src/main/java/jdbc/test/basicjdbc/BasicJdbcApplication.java
+++ src/main/java/jdbc/test/basicjdbc/BasicJdbcApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BasicjdbcApplication {
+public class BasicJdbcApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasicjdbcApplication.class, args);
+        SpringApplication.run(BasicJdbcApplication.class, args);
     }
 
 }
