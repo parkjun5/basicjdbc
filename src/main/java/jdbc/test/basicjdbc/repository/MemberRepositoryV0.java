75ac117956a5f78444dbc16dbd1f7f789895ac12
박세준 <sjpark@selab.co.kr>
2022-10-14 15:25:30 +0900

    [FEA]  DriverManager CRUD examples

--- src/main/java/jdbc/test/basicjdbc/repository/MemberRepositoryV0.java
+++ src/main/java/jdbc/test/basicjdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import jdbc.test.basicjdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -26,6 +27,71 @@ public class MemberRepositoryV0 {
         } catch (SQLException e) {
             log.error("db error", e);
             throw e;
+        } finally {
+            close(conn, pstmt, null);
+        }
+
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from Member where member_id = ?";
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+            if (rs.next()) {
+                Member member = new Member();
+                member.setMemberId(rs.getString("member_id"));
+                member.setMoney(rs.getInt("money"));
+                return member;
+            } else {
+                throw new NoSuchElementException("member not found memberId = " + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(conn, pstmt, rs);
+        }
+
+    }
+
+    public int updateMember(String member_id) {
+        String sql = "update Member set money = 5000 where member_id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, member_id);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            close(conn, pstmt, null);
+        }
+
+    }
+
+    public int deleteMember(String member_id) {
+        String sql = "delete from Member where member_id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, member_id);
+            return pstmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         } finally {
             close(conn, pstmt, null);
         }
